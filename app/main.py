378001981e2b8fc666e941273447d87808e4f483
Jurica Zrna <jurica.zrna@ibm.com>
2020-02-21 23:05:52 +0100

    fix table create

--- app/main.py
+++ app/main.py
@@ -44,7 +44,7 @@ class NumberCollection(Model):
         database = db # This model uses the "people.db" database.
 
 db.connect()
-db.create_tables(NumberCollection)
+db.create_tables([NumberCollection])
 
 @app.route('/')
 def hello():
