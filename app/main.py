0df52c6ddc23cbe766fd223537e4a9fa258c5d21
Jurica Zrna <jurica.zrna@ibm.com>
2020-02-21 21:18:51 +0100

    added templates

--- app/main.py
+++ app/main.py
@@ -1,12 +1,13 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 host = os.getenv('HOSTNAME', 'localhost')
+title = ("Flask on %s" % host)
 
 @app.route('/')
 def hello():
-    return ("Hello World! I'm %s" % host)
+    return render_template('index.html', title=title, host=host)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=PORT)
