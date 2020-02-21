cc539b8ec1977516439d8fab152e4c9d63356214
Jurica Zrna <jurica.zrna@ibm.com>
2020-02-21 21:37:10 +0100

    added db

--- app/main.py
+++ app/main.py
@@ -5,9 +5,18 @@ app = Flask(__name__)
 host = os.getenv('HOSTNAME', 'localhost')
 title = ("Flask on %s" % host)
 
+db_config = {
+  user = os.getenv('databse-user', None),
+  password = os.getenv('databse-password', None),
+  name = os.getenv('databse-name', None),
+  host = os.getenv('databse-host', None),
+  port = os.getenv('databse-port', None)
+}
+
+
 @app.route('/')
 def hello():
-    return render_template('index.html', title=title, host=host)
+    return render_template('index.html', title=title, host=host, db_config = db_config)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=PORT)
